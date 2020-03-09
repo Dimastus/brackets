70856494cda66bb0c9dd706b89488904495d8fbc
Dimasik <dim56026257@yandex.ru>
2020-03-09 23:48:26 +0300

    feat: complete task 'brackets'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strBrackets = bracketsConfig.reduce((acc, cur) => acc + `|${isNaN(cur[0]) ? '\\' + cur[0] : cur[0]}${isNaN(cur[1]) ? '\\' + cur[1] : cur[1]}`, '');
+  let reg = new RegExp(`(${strBrackets.slice(1)})`, 'g');
+  for (let i = 0; i < str.length; i++) {
+    if (reg.test(str)) {
+      str = str.replace(reg, '');
+      i--;
+    }
+  }
+  return str.length ? false : true;
 }
